2a1e9459e22c3947ed97e2a92f5e35ff67fd5c38
HIRAI, Hajime <hirai.hajime@gmail.com>
2010-12-31 02:42:47 +0900

    test

--- experiment/Binout.hs
+++ experiment/Binout.hs
@@ -2,5 +2,14 @@ module Main (main) where
 
 import qualified Data.ByteString as B
 
+import Data.Word
+import Data.Bits
+
+w32toW8 :: Word32 -> [Word8]
+w32toW8 i = [fromIntegral $ (i .&. 0xff000000) `shiftR` 24,
+             fromIntegral $ (i .&. 0x00ff0000) `shiftR` 16,
+             fromIntegral $ (i .&. 0x0000ff00) `shiftR` 8,
+             fromIntegral $ (i .&. 0x000000ff)]
+
 main :: IO ()
-main = B.putStr $ B.pack [0, 1, 2, 3]
+main = B.putStr $ B.pack $ w32toW8 0x00010203
