7b429cad942b45c36030a5b4fdaa20067a935243
HIRAI, Hajime <hirai.hajime@gmail.com>
2010-12-31 02:44:16 +0900

    test

--- experiment/Binout.hs
+++ experiment/Binout.hs
@@ -6,10 +6,10 @@ import Data.Word
 import Data.Bits
 
 w32toW8 :: Word32 -> [Word8]
-w32toW8 i = [fromIntegral $ (i .&. 0xff000000) `shiftR` 24,
-             fromIntegral $ (i .&. 0x00ff0000) `shiftR` 16,
+w32toW8 i = [fromIntegral $ (i .&. 0x000000ff),
              fromIntegral $ (i .&. 0x0000ff00) `shiftR` 8,
-             fromIntegral $ (i .&. 0x000000ff)]
+             fromIntegral $ (i .&. 0x00ff0000) `shiftR` 16,
+             fromIntegral $ (i .&. 0xff000000) `shiftR` 24]
 
 main :: IO ()
 main = B.putStr $ B.pack $ w32toW8 0x00010203
