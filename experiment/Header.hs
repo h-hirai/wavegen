a0e8c4e1813630f0f70478f7f4f1ac2240d5057f
HIRAI, Hajime <hirai.hajime@gmail.com>
2011-01-01 19:32:00 +0900

    test

--- experiment/Header.hs
+++ experiment/Header.hs
@@ -6,19 +6,24 @@ import qualified Data.ByteString.Char8 as C
 import Data.Word
 import Data.Bits
 
-data RIFFchunkDescriptor = RIFF String Word32 String
+class Packable a where
+    toByteString :: a -> B.ByteString
 
-riffToByteString :: RIFFchunkDescriptor -> B.ByteString
-riffToByteString (RIFF chunkID chunkSize format) =
-    C.pack chunkID `B.append`
-    (B.pack $ w32toW8 chunkSize) `B.append`
-    C.pack format
+instance Packable Word32 where
+    toByteString i =
+        fromIntegral (i .&. 0x000000ff) `B.cons`
+        (fromIntegral ((i .&. 0x0000ff00) `shiftR` 8) `B.cons`
+         (fromIntegral ((i .&. 0x00ff0000) `shiftR` 16) `B.cons`
+          (fromIntegral ((i .&. 0xff000000) `shiftR` 24) `B.cons`
+           B.empty)))
 
-w32toW8 :: Word32 -> [Word8]
-w32toW8 i = [fromIntegral $ (i .&. 0x000000ff),
-             fromIntegral $ (i .&. 0x0000ff00) `shiftR` 8,
-             fromIntegral $ (i .&. 0x00ff0000) `shiftR` 16,
-             fromIntegral $ (i .&. 0xff000000) `shiftR` 24]
+data RIFFHeader = RIFF Word32
+
+instance Packable RIFFHeader where
+    toByteString (RIFF chunkSize) =
+        C.pack "RIFF" `B.append`
+        toByteString chunkSize `B.append`
+        C.pack "WAVE"
 
 main :: IO ()
-main = B.putStr $ riffToByteString $ RIFF "RIFF" 4 "WAVE"
+main = B.putStr $ toByteString $ RIFF 4
