095747413cbfa385bd49c70c273b3625e6f71a01
HIRAI, Hajime <hirai.hajime@gmail.com>
2011-01-01 19:06:25 +0900

    test

--- experiment/Binout.hs
+++ experiment/Binout.hs
@@ -12,7 +12,16 @@ w32toW8 i = [fromIntegral $ (i .&. 0x000000ff),
              fromIntegral $ (i .&. 0x00ff0000) `shiftR` 16,
              fromIntegral $ (i .&. 0xff000000) `shiftR` 24]
 
+w32toByteString :: Word32 -> B.ByteString
+w32toByteString i =
+    fromIntegral (i .&. 0x000000ff) `B.cons`
+    (fromIntegral ((i .&. 0x0000ff00) `shiftR` 8) `B.cons`
+     (fromIntegral ((i .&. 0x00ff0000) `shiftR` 16) `B.cons`
+      (fromIntegral ((i .&. 0xff000000) `shiftR` 24) `B.cons`
+       B.empty)))
+
 main :: IO ()
 main = do
   B.putStr $ B.pack $ w32toW8 0x00010203
   B.putStr $ C.pack "String"
+  B.putStr $ w32toByteString 0x04050607
