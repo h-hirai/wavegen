d3565f71f67af0b5295739fcf55daab543ac1ec8
HIRAI, Hajime <hirai.hajime@gmail.com>
2011-01-20 21:01:23 +0900

    first step with a mistake

---
+++ experiment/Data.hs
@@ -0,0 +1,14 @@
+module Main (main) where
+
+import Data.Word
+
+clicks :: Double -> [Double]
+clicks freq = [0, 1/freq..]
+
+sample :: Double -> Double -> Double
+sample f t = sin (2 * pi * f * t)
+
+quantize :: Double -> Word16
+quantize v = truncate $ fromIntegral (maxBound::Word16) * v
+
+main = mapM_ print (take 44100 $ map (quantize . sample 440) (clicks 44100))
