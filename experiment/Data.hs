60b21a6f074422f2ce1b21fe7e604750eb81778e
HIRAI, Hajime <hirai.hajime@gmail.com>
2011-01-31 05:58:46 +0900

    test

--- experiment/Data.hs
+++ experiment/Data.hs
@@ -1,6 +1,17 @@
 module Main (main) where
 
 import Data.Int
+import Data.Bits
+import qualified Data.ByteString as B
+
+class Packable a where
+    toByteString :: a -> B.ByteString
+
+instance Packable Int16 where
+    toByteString i =
+        fromIntegral (i .&. 0x00ff) `B.cons`
+        (fromIntegral ((i .&. 0xff00) `shiftR` 8) `B.cons`
+         B.empty)
 
 clicks :: Double -> [Double]
 clicks freq = [0, 1/freq..]
@@ -11,4 +22,9 @@ sample f t = sin (2 * pi * f * t)
 quantize :: Double -> Int16
 quantize v = truncate $ fromIntegral (maxBound::Int16) * v
 
-main = mapM_ print (take 500 $ map (quantize . sample 440) (clicks 44100))
+main = do
+  let rate = 44100
+      freq = 440
+      wave = map (sample freq) (clicks rate)
+      pack = toByteString . quantize
+  mapM_ B.putStr $ take 100 $ map pack wave
