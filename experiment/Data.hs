8eedf88fe5eb38dc05379d7e4fc81f83bf2a9dab
HIRAI, Hajime <hirai.hajime@gmail.com>
2011-01-30 11:13:32 +0900

    change unsigned to signed

--- experiment/Data.hs
+++ experiment/Data.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Data.Word
+import Data.Int
 
 clicks :: Double -> [Double]
 clicks freq = [0, 1/freq..]
@@ -8,7 +8,7 @@ clicks freq = [0, 1/freq..]
 sample :: Double -> Double -> Double
 sample f t = sin (2 * pi * f * t)
 
-quantize :: Double -> Word16
-quantize v = truncate $ fromIntegral (maxBound::Word16) * ((v + 1) / 2)
+quantize :: Double -> Int16
+quantize v = truncate $ fromIntegral (maxBound::Int16) * v
 
-main = mapM_ print (take 44100 $ map (quantize . sample 440) (clicks 44100))
+main = mapM_ print (take 500 $ map (quantize . sample 440) (clicks 44100))
